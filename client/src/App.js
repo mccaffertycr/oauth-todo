e4e12097676919c317b8d87e1e5728187cbfe267
mccaffertycr <mccaffertycr@gmail.com>
2018-12-01 17:52:37 -0500

    default route

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,45 @@
-import React, { Component } from 'react';
-import Login from './components/login';
-import Profile from './components/profile';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTrashAlt, faPlus, faStickyNote } from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from "react";
+import Login from "./components/login";
+import Profile from "./components/profile";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faTrashAlt,
+  faPlus,
+  faStickyNote
+} from "@fortawesome/free-solid-svg-icons";
 library.add(faTrashAlt, faPlus, faStickyNote);
 
 class App extends Component {
   constructor() {
-    super() 
-    this.state={
+    super();
+    this.state = {
       onMobile: false,
       onTablet: false,
       onDesktop: false,
       isDay: false
-    }
+    };
     this.handleResize = this.handleResize.bind(this);
     this.handleTimeChange = this.handleTimeChange.bind(this);
   }
 
   componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
+    window.addEventListener("resize", this.handleResize);
     this.handleTimeChange();
   }
 
   componentWillUnMount() {
-    window.removeEventListener('resize', this.handleResize);
+    window.removeEventListener("resize", this.handleResize);
   }
 
   handleResize() {
-    const breakpoints = [768, 991, 1199]
+    const breakpoints = [768, 991, 1199];
     if (window.innerWidth <= breakpoints[0]) {
       this.setState({ onMobile: true, onTablet: false, onDesktop: false });
     } else if (window.innerWidth <= breakpoints[1]) {
       this.setState({ onMobile: false, onTablet: true, onDesktop: false });
     } else {
-      this.setState({ onMobile: false, onTablet: false, onDesktop: true })
+      this.setState({ onMobile: false, onTablet: false, onDesktop: true });
     }
   }
 
@@ -44,7 +48,7 @@ class App extends Component {
 
     time = time.toLocaleString();
 
-    if (time.indexOf('PM') <= 0) {
+    if (time.indexOf("PM") <= 0) {
       this.setState({
         isDay: false
       });
@@ -53,30 +57,31 @@ class App extends Component {
         isDay: true
       });
     }
-
   }
 
   render() {
-     const bgStyle = {
-      background: `${this.state.isDay ? 
-                    'url("/assets/img/day_bg.jpg")' : 
-                    'url("/assets/img/night_bg.jpg")'} 
+    const bgStyle = {
+      background: `${
+        this.state.isDay
+          ? 'url("/assets/img/day_bg.jpg")'
+          : 'url("/assets/img/night_bg.jpg")'
+      } 
                     no-repeat center center fixed`,
-      backgroundSize: 'cover',
-      width: '100%',
-      height: '100%',
-      overflow: 'hidden',
-      position: 'fixed',
+      backgroundSize: "cover",
+      width: "100%",
+      height: "100%",
+      overflow: "hidden",
+      position: "fixed",
       top: 0,
       left: 0
-    }
+    };
 
     return (
       <div className="App" style={bgStyle}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Login} />
-            <Route exact path='/profile' component={Profile} />
+            <Route exact path="/profile" component={Profile} />
           </Switch>
         </BrowserRouter>
       </div>
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,89 +1,95 @@
-const passport = require('passport');
-const express = require('express');
-const db = require('../models')
-const app = express();
-
+const passport = require("passport");
+const router = require("express").Router();
+const db = require("../models");
 
 module.exports = app => {
-    app.get('/auth/google', passport.authenticate('google', {
-      scope: ['profile', 'email']
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
     })
-    );
+  );
 
-    app.get('/auth/google/callback', passport.authenticate('google'),(req,res)=>{
-      res.redirect('/profile');
-    });
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/profile");
+    }
+  );
 
-    app.get('/api/logout', (req, res) => {
-      req.logout();
-      res.redirect('/');
-    });
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
 
-    app.get('/api/current_user', (req, res) => {
-      res.send(req.user);
-    });
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 
-    app.get('/api/todos', (req, res) => {
-      if (req.user) {
-        db.User
-          .findOne({ _id: req.user._id })
-          .populate('todos')
-          .then(userDoc => {
-            res.json({ todos: userDoc.todos })
-          })
-      } else {
-        res.status(403).send('you need to be logged in to do that');
-      }
-    })
+  app.get("/api/todos", (req, res) => {
+    if (req.user) {
+      db.User.findOne({ _id: req.user._id })
+        .populate("todos")
+        .then(userDoc => {
+          res.json({ todos: userDoc.todos });
+        });
+    } else {
+      res.status(403).send("you need to be logged in to do that");
+    }
+  });
 
-    app.put('/api/todos', (req, res) => {
-      if (req.user) {
-        if (req.body.data.completed) {
-          db.Todo
-            .updateOne({ _id: req.body.data.id }, { completed: true })
-            .then(() => {
-              res.send('todo completed');
-            })
-            .catch(err => console.log(err));
-        } else if (req.body.data.finished) {
-          db.Todo
-          .updateOne({ _id: req.body.data.id }, { finished: true })
+  app.put("/api/todos", (req, res) => {
+    if (req.user) {
+      if (req.body.data.completed) {
+        db.Todo.updateOne({ _id: req.body.data.id }, { completed: true })
           .then(() => {
-            res.send('todo deleted');
+            res.send("todo completed");
           })
           .catch(err => console.log(err));
-        }
-      } else {
-        res.status(403).send('you need to be logged in to do that');
-      }
-
-    })
-
-    app.post('/api/todos', (req, res) => {
-      if (req.user) {
-        db.Todo
-          .create({ todo: req.body.data.todo })
-          .then(todoDoc => {
-            db.User
-              .updateOne({ _id: req.user._id }, {$push: { todos: todoDoc._id } })
-              .then(() => {
-                res.send('added new todo')
-              })
+      } else if (req.body.data.finished) {
+        db.Todo.updateOne({ _id: req.body.data.id }, { finished: true })
+          .then(() => {
+            res.send("todo deleted");
           })
-      } else {
-        res.status(403).send('you need to be logged in to do that');
+          .catch(err => console.log(err));
       }
+    } else {
+      res.status(403).send("you need to be logged in to do that");
+    }
+  });
+
+  app.post("/api/todos", (req, res) => {
+    if (req.user) {
+      db.Todo.create({ todo: req.body.data.todo }).then(todoDoc => {
+        db.User.updateOne(
+          { _id: req.user._id },
+          { $push: { todos: todoDoc._id } }
+        ).then(() => {
+          res.send("added new todo");
+        });
+      });
+    } else {
+      res.status(403).send("you need to be logged in to do that");
+    }
+  });
+
+  app.get(
+    "/auth/facebook",
+    passport.authenticate("facebook", {
+      profileFields: ["id", "name"]
     })
+  );
 
-    app.get('/auth/facebook', passport.authenticate('facebook', {
-      profileFields: ['id', 'name'],
-    })
-    );
+  app.get(
+    "/auth/facebook/callback",
+    passport.authenticate("facebook"),
+    (req, res) => {
+      res.redirect("/profile");
+    }
+  );
 
-    app.get('/auth/facebook/callback', passport.authenticate('facebook'),(req,res)=>{
-      res.redirect('/profile');
-    });
-
-
-
+  router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 };
